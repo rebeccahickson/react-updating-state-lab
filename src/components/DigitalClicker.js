83b7e4662f77b5f00ca6a84fbb4c3d96e784b0b2
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-02-25 16:45:37 -0500

    commit tree abb4cad59b7cef2977e5a4ec176c31b46406ac4a

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,23 @@
-// Code DigitalClicker Component Here
+import React from "react";
+
+export default class DigitalClicker extends React.Component {
+  state = {
+    timesClicked: 0,
+  };
+
+  handleClick = () => {
+    this.setState((prevState) => {
+      return {
+        timesClicked: prevState.timesClicked + 1,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <button label="Times Clicked" onClick={this.handleClick}>
+        {this.state.timesClicked}
+      </button>
+    );
+  }
+}
